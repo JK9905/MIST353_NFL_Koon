f4e1bc731ffc366e21a08e0c145f168fccf94051
Second Computer <jak00049@mix.wvu.edu>
2026-02-17 13:02:26 -0500

    Inserted data for all 32 teams, altered formatting on createtables.

--- Data/CreateTablesKoon.sql
+++ Data/CreateTablesKoon.sql
@@ -10,25 +10,37 @@ use MIST353_NFL_RDB_Koon;
 
 --drop database to change naming
 --Create tables for the first iteration
-
-
-
+if (OBJECT_ID('Team') IS NOT NULL)
+    DROP TABLE Team;
+if (OBJECT_ID('ConferenceDivision') IS NOT NULL)
+    DROP TABLE ConferenceDivision;
+go
 CREATE TABLE ConferenceDivision(
 ConferenceDivisionID INT identity(1,1)
 constraint PK_ConferenceDivision primary key,
 Conference NVARCHAR(50) NOT NULL
-constraint CK_ConferenceNames CHECK (Conference in ("AFC", "NFC")),
-Divison NVARCHAR(50) NOT NULL
-constraint CK_DivisonNames CHECK (Division IN ('East','North','South','West')),
+constraint CK_ConferenceNames CHECK (Conference in ('AFC', 'NFC')),
+Division NVARCHAR(50) NOT NULL
+constraint CK_DivisionNames CHECK (Division IN ('East','North','South','West')),
+CONSTRAINT UQ_ConferenceDivision UNIQUE (Conference,Division)
 );
+
+--alter table ConferenceDivison
+    --ADD CONSTRAINT UQ_ConferenceDivision UNIQUE (Conference,Division);
+
+--alter table ConferenceDivision
+    --NOCHECK CONSTRAINT CK_ConferenceNames;
+
+----alter table ConferenceDivision
+    --CHECK CONSTRAINT CK_ConferenceNames;
 
 Create table Team (
     --starts at 1 and increments by 1
-    constraint PK_Team primary key,
-TeamID int identity(1,1) not null,
+TeamID int identity(1,1) not null 
+constraint PK_Team primary key,
 TeamName Nvarchar (50) not null,
 TeamCityState Nvarchar(50) not null,
-TeamColors NVARCHAR(50) NOT NULL,
+TeamColors NVARCHAR(100) NOT NULL,
 ConferenceDivisionID INT NOT NULL
     constraint FK_Team_ConferenceDivison foreign key references ConferenceDivision (ConferenceDivisionID),
 TeamLogo NVARCHAR(100)
--- Data/InsertDataKoon.sql
+++ Data/InsertDataKoon.sql
@@ -2,29 +2,58 @@
 --insert all the ConferenceDivison data (8 rows)
 --insert team data for AFC North (4 rows)
 -- team IDs',conferences and divisions
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (1,"AFC","East");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (2,"AFC","North");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (3,"AFC","South");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (4,"AFC","West");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (5,"NFC","East");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (6,"NFC","North");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (7,"NFC","South");
-INSERT INTO ConferenceDivision(ConferenceDivisionID,Conference,Division)
-VALUES (8,"NFC","West");
+USE MIST353_NFL_RDB_Koon;
 
+INSERT INTO ConferenceDivision(Conference,Division)
+VALUES ('AFC','East'),
+       ('AFC','North'),
+       ('AFC','South'),
+       ('AFC','West'),
+       ('NFC','East'),
+       ('NFC','North'),
+       ('NFC','South'),
+       ('NFC','West');
+SELECT * FROM ConferenceDivision
 -- Team information for AFC North
-INSERT INTO Team(TeamID,TeamName,TeamCityState,TeamColors,ConferenceDivisionID,TeamLogo)
-VALUES (1,"Baltimore Ravens","Baltimore, MD","Purple, Black, Metallic Gold",2,"C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Ravens.png");
-INSERT INTO Team(TeamID,TeamName,TeamCityState,TeamColors,ConferenceDivisionID,TeamLogo)
-VALUES (2,"Cincinnati Bengals","Cincinnati, OH","Black, Orange, White",2,"C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Bengals.png");
-INSERT INTO Team(TeamID,TeamName,TeamCityState,TeamColors,ConferenceDivisionID,TeamLogo)
-VALUES (3,"Cleveland Browns","Cleveland, OH","Brown, Orange, White",2,"C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Browns.png");
-INSERT INTO Team(TeamID,TeamName,TeamCityState,TeamColors,ConferenceDivisionID,TeamLogo)
-VALUES (4,"Pittsburgh Steelers","Pittsburgh, PA","Black, Gold, White",2,"C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Steelers.png");
+INSERT INTO Team(TeamName,TeamCityState,TeamColors,ConferenceDivisionID,TeamLogo)
+VALUES ('Baltimore Ravens','Baltimore, MD','Purple, Black, Metallic Gold',2,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Ravens.png'),
+       ('Cincinnati Bengals','Cincinnati, OH','Black, Orange, White',2,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Bengals.png'),
+       ('Cleveland Browns','Cleveland, OH','Brown, Orange, White',2,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Browns.png'),
+       ('Pittsburgh Steelers','Pittsburgh, PA','Black, Gold, White',2,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Steelers.png'),
+       
+       ('Buffalo Bills','Buffalo, NY','Royal Blue, Red, White',1,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Bills.png'),
+       ('New England Patriots','Foxborough, MA','Navy Blue, Red, Silver',1,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Patriots.png'),
+       ('Miami Dolphins','Miami, FL','Aqua, Orange, White',1,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Dolphins.png'),
+       ('New York Jets','East Rutherford, NJ','Green, White',1,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Jets.png'),
+       
+       ('Denver Broncos','Denver, CO','Orange, Navy Blue, White',4,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Broncos.png'),
+       ('Kansas City Chiefs','Kansas City, MO','Red, Gold, White',4,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Chiefs.png'),
+       ('Las Vegas Raiders','Las Vegas, NV','Silver, Black',4,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Raiders.png'),
+       ('Los Angeles Chargers','Los Angeles, CA','Powder Blue, Gold, White',4,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Chargers.png'),
+       
+       ('Jacksonville Jaguars','Jacksonville, FL','Jaguar Blue, Black, White',3,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Jaguars.png'),
+       ('Houston Texans','Houston, TX','Deep Steel Blue, Battle Red, Liberty White',3,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Texans.png'),
+       ('Tennessee Titans','Nashville, TN','Titans Navy, Titans Blue, Titans Red',3,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Titans.png'),
+       ('Indianapolis Colts','Indianapolis, IN','Colts Blue, White',3,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Colts.png'),
+       
+       ('Dallas Cowboys','Dallas, TX','Navy Blue, Metallic Silver, White',5,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Cowboys.png'),
+       ('New York Giants','East Rutherford, NJ','Royal Blue, Red, White',5,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Giants.png'),
+       ('Philadelphia Eagles','Philadelphia, PA','Midnight Green, Silver, Black',5,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Eagles.png'),
+       ('Washington Commanders','Landover, MD','Burgundy, Gold',5,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Commanders.png'),
+       
+       ('Chicago Bears','Chicago, IL','Navy Blue, Orange, White',6,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Bears.png'),
+       ('Detroit Lions','Detroit, MI','Honolulu Blue, Silver, White',6,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Lions.png'),
+       ('Green Bay Packers','Green Bay, WI','Dark Green, Gold, White',6,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Packers.png'),
+       ('Minnesota Vikings','Minneapolis, MN','Purple, Gold, White',6,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Vikings.png'),
+       
+       ('Atlanta Falcons','Atlanta, GA','Red, Black, White',7,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Falcons.png'),
+       ('Carolina Panthers','Charlotte, NC','Panther Blue, Black, Silver',7,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Panthers.png'),
+       ('New Orleans Saints','New Orleans, LA','Old Gold, Black, White',7,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Saints.png'),
+       ('Tampa Bay Buccaneers','Tampa, FL','Red, Pewter, Black, White',7,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Buccaneers.png'),
+       
+       ('Arizona Cardinals','Glendale, AZ','Cardinal Red, White, Black',8,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Cardinals.png'),
+       ('Los Angeles Rams','Los Angeles, CA','Rams Royal, Solor Gold, White',8,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Rams.png'),
+       ('San Francisco 49ers','San Francisco, CA','Scarlet, Gold, White',8,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\49ers.png'),
+       ('Seattle Seahawks','Seattle, WA','College Navy, Action Green, Wolf Grey',8,'C:\Users\koonj\MIST 353\MIST353_NFL_Koon\Team Logos\Seahawks.png');
+
+       SELECT * FROM Team ORDER BY TeamID;
