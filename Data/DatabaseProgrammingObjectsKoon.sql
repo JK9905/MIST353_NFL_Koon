1669a601ac0de9af9c8085980039d12137a6dd49
Second Computer <jak00049@mix.wvu.edu>
2026-02-18 22:43:50 -0500

    Created and tested 3 queries for class.

--- Data/DatabaseProgrammingObjectsKoon.sql
+++ Data/DatabaseProgrammingObjectsKoon.sql
@@ -1,2 +1,20 @@
 --3 QUERIES
---1 EACHFOR CONFERENCEDIVISION AND TEAM TABLES, AND 1 JOIN QUERY
+--1 EACH FOR CONFERENCEDIVISION AND TEAM TABLES, AND 1 JOIN QUERY
+USE MIST353_NFL_RDB_Koon;
+
+-- Query 1: Select all data from ConferenceDivision table
+SELECT * FROM ConferenceDivision
+WHERE Conference = 'NFC'
+AND Division = 'South';
+
+-- Query 2: Select all data from Team table
+SELECT * FROM Team 
+WHERE TeamColors = 'Black, Orange, White'
+AND TeamID = 2;
+
+-- Query 3: Join ConferenceDivision and Team tables to show team information with conference and division details
+SELECT TeamName, TeamCityState, TeamColors, Conference, Division
+FROM Team t
+JOIN ConferenceDivision cd ON t.ConferenceDivisionID = cd.ConferenceDivisionID
+WHERE Conference = 'AFC'
+AND Division = 'North';
