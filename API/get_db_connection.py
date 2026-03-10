ea833bfaf9717a87aec4d35134e33dada5026bd3
Second Computer <jak00049@mix.wvu.edu>
2026-03-10 12:48:01 -0400

    Made changes to API and tested connection using test_get_db_connection.

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -1,7 +1,15 @@
 import os
-import pydoc
-from dotenv import load_dotenv
+import pyodbc
+from dotenv import load_dotenv  
 
 load_dotenv()
 
+def get_db_connection():
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_NAME")
+    username = os.getenv("DB_LOGIN")
+    password = os.getenv("DB_PASSWORD")
+    #ODBC Driver 18 for sql server can only be used in synchronous mode
+    connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=Yes;TrustServerCertificate=Yes;ConnectionTimeout=30;"
+    return pyodbc.connect(connection_string)
 
