ae2a208563d9a0e7242e79c6e6d4dcf7c087dfb5
Second Computer <jak00049@mix.wvu.edu>
2026-04-20 21:52:52 -0400

    Changed get_teams_for_specified_fan_ui

--- UI/get_teams_for_specified_fan_ui.py
+++ UI/get_teams_for_specified_fan_ui.py
@@ -3,21 +3,18 @@ from fetch_data import fetch_data
 
 def get_teams_for_specified_fan_ui():
 
-    st.header("Get Teams for Specified Fan")
+    fan_name = st.session_state.app_user_fullname
+    st.header(f"Get Teams associated with {fan_name}")
 
     nfl_fan_id = st.number_input("Enter NFL Fan ID", min_value=1, step=1)
 
     if st.button("Fetch Teams"):
         input_params = {}
-        fan_name = st.session_state.app_user_fullname
         nfl_fan_id = st.text_input("NFL Fan ID", value=st.session_state.app_user_id, disabled=True)
-        input_params["nfl_fan_id"] = nfl_fan_id
-        
         #define fetch_data function and call with input_params
         df = fetch_data ("get_teams_for_specified_fan/", input_params)
 
         if df is not None and not df.empty:
-            st.subheader(f"Teams associated with {fan_name} (ID: {nfl_fan_id}):")
             st.dataframe(df, use_container_width=True, hide_index=True)
         else:
             st.info(f"No teams found for {fan_name} (ID: {nfl_fan_id}). Please adjust your selection and try again.")
