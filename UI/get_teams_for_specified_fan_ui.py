fbc03bb72419042837299ff5b8c1ab20ed2b8078
Second Computer <jak00049@mix.wvu.edu>
2026-04-20 22:12:28 -0400

    changes to fan ui

--- UI/get_teams_for_specified_fan_ui.py
+++ UI/get_teams_for_specified_fan_ui.py
@@ -3,7 +3,7 @@ from fetch_data import fetch_data
 
 def get_teams_for_specified_fan_ui():
 
-    fan_name = st.session_state.app_user_Fullname
+    fan_name = st.session_state.app_user_fullname
     st.header(f"Get Teams associated with {fan_name}")
 
     input_params = {}
@@ -15,4 +15,4 @@ def get_teams_for_specified_fan_ui():
     if df is not None and not df.empty:
         st.dataframe(df, use_container_width=True, hide_index=True)
     else:
-        st.info(f"No teams found for {fan_name} (ID: {nfl_fan_id}). Please adjust your selection and try again.")
+        st.info("No teams found for the specified fan.")
