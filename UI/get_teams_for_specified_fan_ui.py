14e162a0f72db64248cb3a9a43590e32fb632fc6
Second Computer <jak00049@mix.wvu.edu>
2026-04-20 22:07:28 -0400

    changed fan ui

--- UI/get_teams_for_specified_fan_ui.py
+++ UI/get_teams_for_specified_fan_ui.py
@@ -3,16 +3,16 @@ from fetch_data import fetch_data
 
 def get_teams_for_specified_fan_ui():
 
-    fan_name = st.session_state.app_user_fullname
+    fan_name = st.session_state.app_user_Fullname
     st.header(f"Get Teams associated with {fan_name}")
 
-    if st.button("Fetch Teams"):
-        input_params = {}
-        nfl_fan_id = st.text_input("NFL Fan ID", value=st.session_state.app_user_id, disabled=True)
-        #define fetch_data function and call with input_params
-        df = fetch_data ("get_teams_for_specified_fan/", input_params)
+    input_params = {}
+    nfl_fan_id = st.text_input("NFL Fan ID", value=st.session_state.app_user_id, disabled=True)
+    #define fetch_data function and call with input_params
+    input_params["nfl_fan_id"] = nfl_fan_id
+    df = fetch_data ("get_teams_for_specified_fan/", input_params)
 
-        if df is not None and not df.empty:
-            st.dataframe(df, use_container_width=True, hide_index=True)
-        else:
-            st.info(f"No teams found for {fan_name} (ID: {nfl_fan_id}). Please adjust your selection and try again.")
+    if df is not None and not df.empty:
+        st.dataframe(df, use_container_width=True, hide_index=True)
+    else:
+        st.info(f"No teams found for {fan_name} (ID: {nfl_fan_id}). Please adjust your selection and try again.")
